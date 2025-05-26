39a7c01b2234e13bb426f7627eff5cb4088ef6f3
Ivan250505 <ivvh2505@gmail.com>
2025-05-26 01:13:27 -0500

    error

--- js/ingresar.js
+++ js/ingresar.js
@@ -1,6 +1,5 @@
 import { obtainUsuarios } from "../apiConnection/consumeApi.js";
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("loginForm");
 
@@ -10,43 +9,41 @@ document.addEventListener("DOMContentLoaded", () => {
     const usuariosObtained = await obtainUsuarios();
     const usuario = document.getElementById('usuario').value;
     const contraseña = document.getElementById('contraseña').value;
-    let encontrado = false;
-    let accesoRestringido = false;
+    
+    let usuarioEncontrado = null;
 
     for (const usuarios of usuariosObtained) {
-      const { u_usuario, u_contraseña, u_tipoAcceso } = usuarios;
-
+      const { u_usuario, u_contraseña } = usuarios;
       if (u_usuario === usuario && u_contraseña === contraseña) {
-        encontrado = true;
-
-        if (u_tipoAcceso === "Restringido") {
-          accesoRestringido = true;
-        }
-
+        usuarioEncontrado = usuarios;
         break;
       }
     }
 
-    if (!encontrado) {
-      Swal.fire({
+    if (!usuarioEncontrado) {
+      window.Swal.fire({
         icon: 'error',
         title: 'Usuario o contraseña incorrectos',
         text: 'Verifica tus datos e intenta nuevamente.',
         confirmButtonText: 'Reintentar',
         confirmButtonColor: '#3085d6'
       });
-    } else if (accesoRestringido) {
-      Swal.fire({
+      return;
+    }
+
+    if (usuarioEncontrado.u_tipoAcceso === "Restringido") {
+      window.Swal.fire({
         icon: 'warning',
         title: 'Acceso restringido',
         text: 'No tienes permiso para acceder al sistema.',
         confirmButtonText: 'Entendido',
         confirmButtonColor: '#3085d6'
       });
-    } else {
-      localStorage.setItem("Usuario", usuario);
-      localStorage.setItem("tipoacceso", usuariosObtained.find(u => u.u_usuario === usuario).u_tipoAcceso);
-      window.location.href = "index.html";
+      return;
     }
+
+    localStorage.setItem("Usuario", usuarioEncontrado.u_usuario);
+    localStorage.setItem("tipoacceso", usuarioEncontrado.u_tipoAcceso);
+    window.location.href = "index.html";
   });
 });
